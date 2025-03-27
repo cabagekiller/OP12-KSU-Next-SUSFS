52ca2eca19078dbf7cc1160ceb2fd97e96ccb7b2
cabagekiller <cabagekiller@gmail.com>
2025-03-26 22:20:43 -0400

    Create atomic-fallbacks.h

---
+++ patches/atomic-fallbacks.h
@@ -0,0 +1,58 @@
+#ifndef _LINUX_ATOMIC_FALLBACKS_H
+#define _LINUX_ATOMIC_FALLBACKS_H
+
+#ifndef ALWAYS_INLINE
+#define ALWAYS_INLINE inline __attribute__((always_inline))
+#endif
+
+static ALWAYS_INLINE long raw_atomic_long_add_return(long i, atomic_long_t *v)
+{
+    atomic_long_t temp = *v;
+    *v += i;
+    return temp;
+}
+
+static ALWAYS_INLINE long raw_atomic64_add_return(long i, atomic64_t *v)
+{
+    atomic64_t temp = *v;
+    atomic64_add(i, v);
+    return temp;
+}
+
+static ALWAYS_INLINE long raw_atomic_long_add_return_acquire(long i, atomic_long_t *v)
+{
+    return raw_atomic_long_add_return(i, v);
+}
+
+static ALWAYS_INLINE long raw_atomic64_add_return_acquire(long i, atomic64_t *v)
+{
+    atomic64_t temp = *v;
+    atomic64_add(i, v);
+    return temp;
+}
+
+static ALWAYS_INLINE long raw_atomic_long_add_return_release(long i, atomic_long_t *v)
+{
+    return raw_atomic_long_add_return(i, v);
+}
+
+static ALWAYS_INLINE long raw_atomic64_add_return_release(long i, atomic64_t *v)
+{
+    atomic64_t temp = *v;
+    atomic64_add(i, v);
+    return temp;
+}
+
+static ALWAYS_INLINE long raw_atomic_long_add_return_relaxed(long i, atomic_long_t *v)
+{
+    return raw_atomic_long_add_return(i, v);
+}
+
+static ALWAYS_INLINE long raw_atomic64_add_return_relaxed(long i, atomic64_t *v)
+{
+    atomic64_t temp = *v;
+    atomic64_add(i, v);
+    return temp;
+}
+
+#endif /* _LINUX_ATOMIC_FALLBACKS_H */
