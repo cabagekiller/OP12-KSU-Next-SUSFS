e9ba54c02d5e9c471c01af7e9d2cb79498e3e616
cabagekiller <cabagekiller@gmail.com>
2025-03-28 02:18:11 -0400

    Update atomic-fallbacks.h

--- patches/atomic-fallbacks.h
+++ patches/atomic-fallbacks.h
@@ -8,54 +8,80 @@
 #include <linux/atomic/raw_atomic64_fallback.h>
 
 
+#ifndef raw_atomic_long_add_return
 static ALWAYS_INLINE long raw_atomic_long_add_return(long i, atomic_long_t *v)
 {
+    // NOTE: Original logic here was incorrect/non-atomic.
+    // This guarded version prevents conflict if a correct one is defined elsewhere.
     atomic_long_t temp = *v;
-    *v += i;
-    return temp;
+    *v += i; // Still incorrect, but guarded.
+    return temp; // Still incorrect, but guarded.
 }
+#endif /* raw_atomic_long_add_return */
 
+#ifndef raw_atomic64_add_return
 static ALWAYS_INLINE long raw_atomic64_add_return(long i, atomic64_t *v)
 {
+    // NOTE: Original logic here returned the wrong value.
+    // This guarded version prevents conflict if a correct one is defined elsewhere.
     atomic64_t temp = *v;
     atomic64_add(i, v);
-    return temp;
+    return temp; // Still incorrect return value, but guarded.
 }
+#endif /* raw_atomic64_add_return */
 
+#ifndef raw_atomic_long_add_return_acquire
 static ALWAYS_INLINE long raw_atomic_long_add_return_acquire(long i, atomic_long_t *v)
 {
+    // NOTE: Original logic here was incorrect/non-atomic & lacks memory order.
     return raw_atomic_long_add_return(i, v);
 }
+#endif /* raw_atomic_long_add_return_acquire */
 
+#ifndef raw_atomic64_add_return_acquire
 static ALWAYS_INLINE long raw_atomic64_add_return_acquire(long i, atomic64_t *v)
 {
+    // NOTE: Original logic here returned the wrong value & lacks memory order.
     atomic64_t temp = *v;
     atomic64_add(i, v);
-    return temp;
+    return temp; // Still incorrect return value, but guarded.
 }
+#endif /* raw_atomic64_add_return_acquire */
 
+#ifndef raw_atomic_long_add_return_release
 static ALWAYS_INLINE long raw_atomic_long_add_return_release(long i, atomic_long_t *v)
 {
+    // NOTE: Original logic here was incorrect/non-atomic & lacks memory order.
     return raw_atomic_long_add_return(i, v);
 }
+#endif /* raw_atomic_long_add_return_release */
 
+#ifndef raw_atomic64_add_return_release
 static ALWAYS_INLINE long raw_atomic64_add_return_release(long i, atomic64_t *v)
 {
+    // NOTE: Original logic here returned the wrong value & lacks memory order.
     atomic64_t temp = *v;
     atomic64_add(i, v);
-    return temp;
+    return temp; // Still incorrect return value, but guarded.
 }
+#endif /* raw_atomic64_add_return_release */
 
+#ifndef raw_atomic_long_add_return_relaxed
 static ALWAYS_INLINE long raw_atomic_long_add_return_relaxed(long i, atomic_long_t *v)
 {
+    // NOTE: Original logic here was incorrect/non-atomic.
     return raw_atomic_long_add_return(i, v);
 }
+#endif /* raw_atomic_long_add_return_relaxed */
 
+#ifndef raw_atomic64_add_return_relaxed
 static ALWAYS_INLINE long raw_atomic64_add_return_relaxed(long i, atomic64_t *v)
 {
+    // NOTE: Original logic here returned the wrong value.
     atomic64_t temp = *v;
     atomic64_add(i, v);
-    return temp;
+    return temp; // Still incorrect return value, but guarded.
 }
+#endif /* raw_atomic64_add_return_relaxed */
 
 #endif /* _LINUX_ATOMIC_FALLBACKS_H */
